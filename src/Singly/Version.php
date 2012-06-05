acc8a1f8528db418fd34e41365b66e7475c791cc
Tom Anderson <tom.h.anderson@gmail.com>
2012-06-04 19:45:31 -0700

    Added version, consider this the official release.

---
+++ src/Singly/Version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Singly Authentication
+ *
+ * @author Tom Anderson <tom.h.anderson@gmail.com
+ * @license MIT
+ */
+
+namespace Singly;
+
+final class Version 
+{
+    const VERSION = '1.0';
+}
